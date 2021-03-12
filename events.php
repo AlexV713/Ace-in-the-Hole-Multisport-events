2ee1ac4a6c3bd95ccb1180e69a66ec8e9e4fe423
Alex Valerio <62916245+AlexV713@users.noreply.github.com>
2021-03-12 15:44:26 -0800

    Styled the Events page for desktop
    I finished styling the events page for desktop view. I also changed some elements to be more easy to read.

--- events.php
+++ events.php
@@ -3,11 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds">
+    <meta name="description"
+        content="Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Open+Sans&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Open+Sans&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/cf72e27b3d.js" crossorigin="anonymous"></script>
     <script src="scripts.js"></script>
     <title>Events Information</title>
@@ -21,26 +22,18 @@
 
         <div id="wrapper">
 
-            <h1>Event Information</h1>
+            <h2 class="center-heading">Events Information</h2>
 
-            <nav id="quick-links">
-                <a href="#includes">Cost Includes</a>
-                <a href="#details">Course Details</a>
-                <a href="#bring">What to Bring</a>
-                <a href="#packet">Packet Pick-up</a>
-                <a href="#faq-heading">FAQ's</a>
-            </nav>
+            <h3>Saturday Events (June 20th)</h3>
 
-            <h2 class="days">Saturday Events (June 20th)</h2>
-
-            <table id="saturdayTable">
+            <table>
                 <tbody>
                     <tr>
                         <th>Event</th>
-                        <td>Long Course Triathlon</td>
-                        <td>Olympic Triathlon</td>
-                        <td>10K</td>
-                        <td>Half Marathon</td>
+                        <td class="event">Long Course Triathlon</td>
+                        <td class="event">Olympic Triathlon</td>
+                        <td class="event">10K</td>
+                        <td class="event">Half Marathon</td>
                     </tr>
                     <tr>
                         <th>Time</th>
@@ -59,14 +52,14 @@
                 </tbody>
             </table>
 
-            <h2 class="days">Sunday Events (June 21st)</h2>
-            <table id="sundayTable">
+            <h3>Sunday Events (June 21st)</h3>
+            <table>
                 <tbody>
                     <tr>
                         <th>Event</th>
-                        <td>Sprint Triathlon</td>
-                        <td>Try-a-Tri</td>
-                        <td>Splash n' Dash</td>
+                        <td class="event">Sprint Triathlon</td>
+                        <td class="event">Try-a-Tri</td>
+                        <td class="event">Splash n' Dash</td>
                     </tr>
                     <tr>
                         <th>Time</th>
@@ -83,7 +76,8 @@
                 </tbody>
             </table>
 
-            <h3 id="includes">Cost Includes</h3>
+            <h3 class="info-section">Cost Includes</h3>
+
             <ul>
                 <li>Food & Beer</li>
                 <li>Access to the weekends's live entertainment & Fitness Expo</li>
@@ -94,96 +88,123 @@
             </ul>
             <p><i>Tech shirts guaranteed to pre-registered participants only</i></p>
 
-            <h2 id="details">Course Details</h2>
-            <ul id="detailsList">
-                <li><strong><span class="titles">Long Course SWIM - 1.2mi:</span></strong> Participants will make two counter-clockwise loops. Large
-                    buoys will mark
-                    the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support
-                    will be present on the beach.</li>
-                <li><strong><span class="titles">Long Course BIKE - 58 Miles:</span></strong> A scenic point-to-point course that travels over
-                    gently rolling hills
-                    prior to three hard climbs. The bike course will be marked with large directional signage and there
-                    will be course marshals at key intersections to help direct you.</li>
-                <li><strong><span class="titles">Long Course RUN - 13.1mi:</span></strong> A mostly flat loop course on widely paved bike paths that
-                    traverse
-                    through and around this beautiful and scenic destination resort (two hills total with a minimal
-                    elevation gain).</li>
-                <li><strong><span class="titles">OLYMPIC SWIM - 1,500 meters:</span></strong> Participants will make two counter-clockwise loops.
-                    Large buoys will
-                    mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical
-                    support will be present on the beach.</li>
-                <li><strong><span class="titles">OLYMPIC BIKE - 28mi:</span></strong> A scenic point-to-point course that travels over gently
-                    rolling hills. The
-                    bike course will be marked with large directional signage and course marshals will be present at key
-                    intersections.</li>
-                <li><strong><span class="titles">OLYMPIC RUN - 10K:</span></strong> A mostly flat loop course on widely paved bike paths that
-                    traverse through and
-                    around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).
-                </li>
-                <li><strong><span class="titles">Sprint:</span></strong> Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike
-                    Course as the Olympic
-                    distance and a 5km run.</li>
-                <li><strong><span class="titles">Try-A-Tri:</span></strong> This novice race is designed for the first time triathlete, those new to
-                    the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance,
-                    (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2
-                    loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).
-                </li>
-                <li><strong><span class="titles">Half Marathon COURSE:</span></strong> Half-Marathon (13.1-miles): Half-Marathon event starts and
-                    finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can
-                    enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment.
-                    Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin
-                    once the results have been certified</li>
-                <li><strong><span class="titles">10k COURSE:</span></strong> The 10K event starts and finishes in the Athletes Village. The paths
-                    are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has
-                    only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to
-                    the roaring cheers of the crowd.</li>
-                <li><strong><span class="titles">Splash n' Dash:</span></strong> Kids event for participants ages 4 to 15. This is a 100 yard swim
-                    and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.</li>
-            </ul>
+            <h3 class="info-section">Course Details</h3>
 
-            <h2 id="bring">What to Bring</h2>
-            <p>Watch the weather closely. The show goes on no matter what the weather is doing. 
-                <br><br>You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip timing
+            <p><strong><span class="titles">Long Course SWIM - 1.2mi:</span></strong><br> Participants will make two
+                counter-clockwise loops. Large
+                buoys will mark
+                the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support
+                will be present on the beach.
+                <br><br>
+                <strong><span class="titles">Long Course BIKE - 58 Miles:</span></strong> A scenic point-to-point
+                course that travels over
+                gently rolling hills
+                prior to three hard climbs. The bike course will be marked with large directional signage and there
+                will be course marshals at key intersections to help direct you.
+                <br><br>
+                <strong><span class="titles">Long Course RUN - 13.1mi:</span></strong> A mostly flat loop course on
+                widely paved bike paths that
+                traverse
+                through and around this beautiful and scenic destination resort (two hills total with a minimal
+                elevation gain).
+                <br><br>
+                <strong><span class="titles">OLYMPIC SWIM - 1,500 meters:</span></strong> Participants will make two
+                counter-clockwise loops.
+                Large buoys will
+                mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical
+                support will be present on the beach.
+                <br><br>
+                <strong><span class="titles">OLYMPIC BIKE - 28mi:</span></strong> A scenic point-to-point course
+                that travels over gently
+                rolling hills. The
+                bike course will be marked with large directional signage and course marshals will be present at key
+                intersections.
+                <br><br>
+                <strong><span class="titles">OLYMPIC RUN - 10K:</span></strong> A mostly flat loop course on widely
+                paved bike paths that
+                traverse through and
+                around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).
+                <br><br>
+                <strong><span class="titles">Sprint:</span></strong> Course will offer a 1-loop 1/2 mile swim, the
+                exact same 28 mile Bike
+                Course as the Olympic
+                distance and a 5km run.
+                <br><br>
+                <strong><span class="titles">Try-A-Tri:</span></strong> This novice race is designed for the first
+                time triathlete, those new to
+                the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance,
+                (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2
+                loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).
+                <br><br>
+                <strong><span class="titles">Half Marathon COURSE:</span></strong> Half-Marathon (13.1-miles):
+                Half-Marathon event starts and
+                finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can
+                enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment.
+                Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin
+                once the results have been certified
+                <br><br>
+                <strong><span class="titles">10k COURSE:</span></strong> The 10K event starts and finishes in the
+                Athletes Village. The paths
+                are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has
+                only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to
+                the roaring cheers of the crowd.
+                <br><br>
+                <strong><span class="titles">Splash n' Dash:</span></strong> Kids event for participants ages 4 to
+                15. This is a 100 yard swim
+                and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.
+            </p>
+
+            <h3 class="info-section">What to Bring</h3>
+
+            <p>Watch the weather closely. The show goes on no matter what the weather is doing.
+                <br><br>You must wear your chip timing piece during the entire event. You will be given a band that will
+                hold your chip timing
                 piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to have
                 your Chip Timing piece on before you start the race and be sure to step over the timing mats after each
                 segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit
-                otherwise, you will have a very difficult time getting off your wet suit. 
-                <br><br><strong><span class="titles">Swim:</span></strong> Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no
+                otherwise, you will have a very difficult time getting off your wet suit.
+                <br><br><strong><span class="titles">Swim:</span></strong><br> Wetsuits are optional for the swim but
+                will provide buoyancy and warmth. However, many people opt for no
                 wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want to bring
                 your own goggles. <br><br>
-                <strong><span class="titles">Bike:</span></strong> A biking helmet is mandatory. You will also receive 2
+                <strong><span class="titles">Bike:</span></strong><br> A biking helmet is mandatory. You will also
+                receive 2
                 stickers in your package with your race number on them. The small sticker should go on the front of your
                 helmet. The bigger sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For
-                safety reasons, be sure to have end-caps on the end of your handle-bars. 
-                <br><br><strong><span class="titles">Run:</span></strong> You must finish the race with your bib number on the front of you. Some people choose to pin it on at
+                safety reasons, be sure to have end-caps on the end of your handle-bars.
+                <br><br><strong><span class="titles">Run:</span></strong><br> You must finish the race with your bib
+                number on the front of you. Some people choose to pin it on at
                 the beginning of the race and have it on for the whole event so they don't have to worry about it.
                 Others pin it on a singlet that they put on once they finish the swim before they head out for the bike.
                 Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before
-                they leave for the run. Use whatever option feels best for you. 
+                they leave for the run. Use whatever option feels best for you.
                 <br><br>Remember to bring a change of
                 clothing so you can enjoy the post-event festivities.</p>
 
-            <h2 id="packet">Packet Pick Up</h2>
-            <p>All packet pick up will occur at <span class="important">Why Worry Racing, 123 NW Everett, Portland OR.</span> Packet pick up hours:
-                Thursday 8-5, Friday 9-noon. No day of event packet pick up.</p>
+            <h3 class="info-section">Packet Pick Up</h3>
+
+            <p><span class="important">All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland
+                    OR. Packet pick up hours:
+                    Thursday 8-5, Friday 9-noon. No day of event packet pick up.</span></p>
             <p><strong>COURSE DETAILS
                     WATER TEMPERATURE is expected to be between 62 & 66 degrees. The temperature will be taken on Friday
                     and the morning of the race. Wetsuits are recommended.</strong></p>
 
-            <h2 id="faq-heading">FAQ's</h2>
-            <p id="faq-small"><strong class="questions">What are the Rules?</strong> We currently adhere to the USAT
+            <h3 class="info-section">FAQ's</h3>
+
+            <p><strong class="questions">What are the Rules?</strong><br> We currently adhere to the USAT
                 Rules for Triathlon &
                 Duathlon.
                 Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed
                 during
                 the bike for obvious safety reasons. <br><br><strong class="questions">Can I use a personal music device
                     while
-                    cycling?</strong> Absolutely no music devices are allowed during the bike segment for obvious
+                    cycling?</strong><br> Absolutely no music devices are allowed during the bike segment for obvious
                 safety
                 risks and will result in immediate disqualification. <br><br><strong class="questions">Can I use a
                     personal music
                     device
-                    while running?</strong> During any running segments, we prefer for athletes to NOT
+                    while running?</strong><br> During any running segments, we prefer for athletes to NOT
                 use music
                 devices
                 to maximize safety, assure a fair competitive environment and assure athletes can hear all course
@@ -192,7 +213,7 @@
                 the challenge of running. To accommodate the needs of our athletes while still assuring maximum
                 safety
                 and a fair, competitive environment, here is the arrangement that we can accommodate.</p>
-            <ul>
+            <ul id="earbuds">
                 <li>If you chose to use a personal music device, you must always keep one ear open for instructions
                     so
                     only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be
@@ -205,18 +226,20 @@
                 <li>We also request that if you chose to use a personal music device that you keep the volume to a
                     minimum to assure all instructions can be heard.</li>
             </ul>
-            <p><strong class="questions">Do I need to wear a wetsuit?</strong> No, you do not need to wear a wetsuit.
+            <p><strong class="questions">Do I need to wear a wetsuit?</strong><br> No, you do not need to wear a
+                wetsuit.
                 Many will not
                 wear a
                 wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor.
-                <br><br><strong class="questions">Do I have to use a road or racing bike?</strong> No. We welcome any
+                <br><br><strong class="questions">Do I have to use a road or racing bike?</strong><br> No. We welcome
+                any
                 type of bike as
                 long
                 as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
 
         </div>
 
-        <button onclick="topFunction()" id="up-button" title="Go to top">Top</button> 
+        <button onclick="topFunction()" id="up-button" title="Go to top">Top</button>
 
     </main>
 
