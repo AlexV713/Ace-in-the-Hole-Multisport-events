3e8f034e3410efb72d6f8cf24c70ec7cf08bcc93
Alex Valerio <62916245+AlexV713@users.noreply.github.com>
2021-03-11 17:20:22 -0800

    Created Desktop Header & Nav
    I changed the links in the navbar to be one word only to space them out. I also finished styling it for desktops.

--- includes/header.html.php
+++ includes/header.html.php
@@ -1,4 +1,4 @@
-<?php define('BASE_URL', '/repositories/Ace-in-the-Hole-Multisport-events/'); ?> 
+<?php define('BASE_URL', '/repositories/Ace-in-the-Hole-Multisport-events/'); ?>
 
 <header>
     <a href="javascript:void(0);" class="icon" onclick="dropLinks()">
@@ -7,12 +7,15 @@
 
     <nav id="desktop-nav">
         <a href="<?php echo BASE_URL; ?>index.php" class="logo">
-            <img src="<?php echo BASE_URL; ?>images/logo2.jpg" id="desktop-logo" alt="logo">
+            <h1>Ace in The Hole</h1>
+            <h3>Multisport Events</h3>
         </a>
-        <a href="<?php echo BASE_URL; ?>index.php">Home</a>
-        <a href="<?php echo BASE_URL; ?>events.php">Events Information</a>
-        <a href="<?php echo BASE_URL; ?>register">Register</a>
-        <a href="<?php echo BASE_URL; ?>contact">Contact</a>
+        <div id="desktop-links">
+            <a href="<?php echo BASE_URL; ?>index.php">Home</a>
+            <a href="<?php echo BASE_URL; ?>events.php">Information</a>
+            <a href="<?php echo BASE_URL; ?>register">Register</a>
+            <a href="<?php echo BASE_URL; ?>contact">Contact</a>
+        </div>
     </nav>
 
     <nav id="mobile-nav">
